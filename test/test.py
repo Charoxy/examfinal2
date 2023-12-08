7c5b8135f18dd8f9642c4ed0790fd760ba9aa737
maxence.guette <maxence.guette@ensweet.fr>
2023-12-08 14:25:47 +0100

    init

--- test/test.py
+++ test/test.py
@@ -1,30 +1,28 @@
 import unittest
 
+def fizzbuzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
+    else:
+        return str(number)
 
 class TestStringMethods(unittest.TestCase):
 
-    def fizzbuzz(number):
-        if number % 3 == 0 and number % 5 == 0:
-            return "FizzBuzz"
-        elif number % 3 == 0:
-            return "Fizz"
-        elif number % 5 == 0:
-            return "Buzz"
-        else:
-            return str(number)
-
     def test_fizz(self):
-        self.assertEqual(self.fizzbuzz(3), "Fizz")
+        self.assertEqual(fizzbuzz(3), "Fizz")
 
     def test_buzz(self):
-        self.assertEqual(self.fizzbuzz(5), "Buzz")
+        self.assertEqual(fizzbuzz(5), "Buzz")
 
     def test_fizzBuzz(self):
-        self.assertEqual(self.fizzbuzz(15), "FizzBuzz")
+        self.assertEqual(fizzbuzz(15), "FizzBuzz")
 
-    def test_fizzBuzz(self):
-        self.assertEqual(self.fizzbuzz(7), "7")
-
+    def test_none(self):
+        self.assertEqual(fizzbuzz(7), "7")
 
 
 if __name__ == '__main__':
